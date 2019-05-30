57a8647e58917de374e02aff8809de6da5a61f78
Nicxes <nicxesh@gmail.com>
2019-05-30 18:36:06 -0300

    Adding navbar component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import Navbar from '../components/navbar'
 import Page from '../components/page'
 
 function Home() {
   return (
     <>
+      <Navbar/>
       <Page title="Hello World">
         <h1>Hello World</h1>
       </Page>
