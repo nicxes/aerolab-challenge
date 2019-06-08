86c4ad68e8bb89219f840256590f7c56a96bc8ce
Nicxes <nicxesh@gmail.com>
2019-06-08 11:44:48 -0300

    Adding global styles and fonts

--- components/page.js
+++ components/page.js
@@ -10,14 +10,27 @@ function Page ({ title, description, children }) {
         <meta name="keywords" content="Aerolab"/>
         <meta name="author" content="@Nicxes"/>
         
+        <meta name="theme-color" content="#0ad4fa"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700&display=swap" rel="stylesheet" />
+
       </Head>
 
       {children}
 
       <style jsx global>
         {`
+          body {
+            font-family: 'Source Sans Pro', sans-serif;
 
+            margin: 0;
+          }
+          .container {
+            max-width: 1240px;
+            margin: 0 auto;
+            padding: 0 20px;
+          }
         `}
       </style>
     </div>
