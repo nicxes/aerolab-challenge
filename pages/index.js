0bc8244291077120c6cb34be344aa9081a0728e2
Nicxes <nicxesh@gmail.com>
2019-06-09 00:22:19 -0300

    Adding header component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-import Navbar from '../components/navbar'
 import Page from '../components/page'
+
+import Navbar from '../components/navbar'
+import Header from '../components/header'
 
 export default class Home extends React.Component {
   render () {
     return (
       <>
-        <Navbar size="36px" color="black"/>
-        <Page title="Reward | Aerolab">
-          
+        <Navbar size="36px" color="#ff7b00" position="fixed"/>
+        <Page title="Rewards | Aerolab">
+          <Header/>
         </Page>
       </>
     )
