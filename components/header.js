2ddc7c18a57eea8345935f16d231449182dcb86f
Nicxes <nicxesh@gmail.com>
2019-06-09 00:22:08 -0300

    Making a component header

---
+++ components/header.js
@@ -0,0 +1,18 @@
+export default function Header(){
+  return(
+    <>
+      <header className="header">
+
+      </header>
+
+      <style jsx>{`
+        .header {
+          position: relative;
+          background: #eee;
+          
+          height: calc(100vh - 300px);
+        }
+      `}</style>
+    </>
+  )
+}
