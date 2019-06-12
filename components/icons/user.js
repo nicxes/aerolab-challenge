bb9ee7ac9247c14ea226cd1e4348e38dc8c8222c
Nicxes <nicxesh@gmail.com>
2019-06-12 17:59:44 -0300

    FIX: Removing some warnings

--- components/icons/user.js
+++ components/icons/user.js
@@ -1,6 +1,6 @@
 export default function Icon({ size }) {
   return (
-    <svg height={size || '100'} viewBox="0 0 24 22" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+    <svg height={size || '100'} viewBox="0 0 24 22" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
       <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
       <circle cx="12" cy="7" r="4"></circle>
     </svg>
