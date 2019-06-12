325d0f519aea4e4112ecb19270c630aee8e7e246
Nicxes <nicxesh@gmail.com>
2019-06-12 17:59:32 -0300

    New color and fixing some warnings

--- components/icons/menu.js
+++ components/icons/menu.js
@@ -1,6 +1,6 @@
 export default function Icon({ size }) {
   return (
-    <svg height={size || '100'} viewBox="0 0 24 22" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <svg height={size || '100'} viewBox="0 0 24 22" fill="none" stroke="#ff7b00" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
       <line x1="3" y1="12" x2="21" y2="12"></line>
       <line x1="3" y1="6" x2="21" y2="6"></line>
       <line x1="3" y1="18" x2="21" y2="18"></line>
