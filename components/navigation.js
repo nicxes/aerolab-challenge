e56628fbbcc56b414cb0a48f85141a624ed8bfc2
Nicxes <nicxesh@gmail.com>
2019-06-16 00:00:03 -0300

    Adding explore category to mobile menu

--- components/navigation.js
+++ components/navigation.js
@@ -1,10 +1,74 @@
 import Link from 'next/link'
 
+import IconSales from './icons/sales'
+import IconCloth from './icons/clothes'
+import IconTech from './icons/tech'
+import IconArt from './icons/art'
+import IconRandom from './icons/random'
+
 export default class Navigation extends React.Component {
   render() {
     return (
       <nav className={this.props.open ? "navigation open": "navigation"}>
         <div className="container">
+          
+          <h5 className="title">Explore</h5>
+          <ul className="explore">
+            <li>
+              <Link href="/#">
+                <a>
+                  <div className="btn">
+                    <IconSales size="35" color="#ff7b00"/>
+                    <span>Discounts</span>
+                  </div>
+                </a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/#">
+                <a>
+                  <div className="btn">
+                    <IconCloth size="35" color="#ff7b00"/>
+                    <span>Appareal</span>
+                  </div>
+                </a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/#">
+                <a>
+                  <div className="btn">
+                    <IconTech size="35" color="#ff7b00"/>
+                    <span>Tech</span>
+                  </div>
+                </a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/#">
+                <a>
+                  <div className="btn">
+                    <IconArt size="35" color="#ff7b00"/>
+                    <span>Art</span>
+                  </div>
+                </a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/#">
+                <a>
+                  <div className="btn">
+                    <IconRandom size="35" color="#ff7b00"/>
+                    <span>Random</span>
+                  </div>
+                </a>
+              </Link>
+            </li>
+          </ul>
 
         </div>
         <style jsx>{`
@@ -25,6 +89,35 @@ export default class Navigation extends React.Component {
           .navigation.open {
             height: calc(100% - 71px);
             opacity: 1;
+          }
+          .title {
+            color: #838383;
+            font-size: 15px;
+            font-weight: 600;
+
+            margin: 25px 0 5px 0;
+          }
+          .explore {
+            padding: 0;
+            margin: 25px 0;
+
+            display: grid;
+            grid-template-columns: repeat(10,25%);
+            justify-items: center;
+            align-items: center;
+            text-align: center;
+
+            overflow: auto;
+          }
+          .explore li {list-style: none;}
+          .explore li a {
+            color: #838383;
+            font-size: 15px;
+            font-weight: 400;
+          }
+          .explore span {
+            display: block;
+            margin: 5px 0 0 0;
           }
         `}</style>
       </nav>
