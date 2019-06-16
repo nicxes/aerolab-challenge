0cdfc2ebca711934debee72e6ae4122ed60bb6dc
Nicxes <nicxesh@gmail.com>
2019-06-16 19:32:52 -0300

    Getting enviroments variables for development

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('now-env')
+}
+
 module.exports = {
-  target: 'serverless'
+  target: 'serverless',
+  env: {
+    URL: process.env.AEROLAB_URL,
+    TOKEN: process.env.AEROLAB_TOKEN
+  }
 }
