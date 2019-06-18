6fe8cc6653d5bdc41b68946c2fcd6b4e2f432f30
Nicxes <nicxesh@gmail.com>
2019-06-18 06:09:57 -0300

    Create arrow-down.js

---
+++ components/icons/arrow-down.js
@@ -0,0 +1,7 @@
+export default function Icon({ size, color }) {
+  return (
+    <svg viewBox="0 0 404.257 404.257" height={size || '100'} fill={color}>
+      <path d="M386.257 114.331L202.128 252.427 18 114.331l-18 24 202.128 151.596 202.129-151.596z" />
+    </svg>
+  )
+}
