5596c8aad9e241d5316982010edab55b189b5a75
Nicxes <nicxesh@gmail.com>
2019-06-18 00:32:17 -0300

    Making explore and adding some basic intro page

---
+++ pages/explore.js
@@ -0,0 +1,54 @@
+import Page from '../components/page'
+
+import Navbar from '../components/navbar'
+import Navigation from '../components/navigation'
+import Intro from '../components/intro'
+
+import API from '../api/aerolab'
+
+export default class Home extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      name: "",
+      bits: 0,
+      open: false
+    }
+    this.handleMenu = this.handleMenu.bind(this);
+  }
+  handleMenu(){
+    this.setState({open: !this.state.open})
+  }
+  componentDidMount() {
+    API.get(`user/me`)
+      .then((res) => {
+        this.setState({
+          name: res.data.name,
+          bits: res.data.points
+        })
+      })
+      .catch(err => console.log(err))
+  }
+  render () {
+    return (
+      <>
+        <Navbar size="36px" color="#ff7b00" open={this.state.open} handleMenu={this.handleMenu} bits={this.state.bits}/>
+        <Navigation open={this.state.open}/>
+        <Page
+          title="Store | Aerolab"
+          description="We're a Digital Product Agency that believes in combining user-centered design with cutting-edge technology to build products that make people happy.">
+          
+          <Intro/>
+        
+        </Page>
+        
+        <style jsx global>{`
+          body {
+            overflow: ${this.state.open ? "hidden": "auto"}
+          }
+        `}</style>
+      </>
+    )
+  }
+}
